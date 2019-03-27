8b9c15b36a64ce622c334d3b663a7cb208d1dbdb
Chris Erin <chris.erin@gmail.com>
2019-03-27 10:54:39 -0500

    boom

---
+++ precache-manifest.9029559d537570139e53a2c2278509b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "767a73a421ab3e2e7118",
+    "url": "./static/css/main.fb7149d0.chunk.css"
+  },
+  {
+    "revision": "767a73a421ab3e2e7118",
+    "url": "./static/js/main.21d320e7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1c1f5129d93a6fa0fbd7",
+    "url": "./static/css/2.27af09c9.chunk.css"
+  },
+  {
+    "revision": "1c1f5129d93a6fa0fbd7",
+    "url": "./static/js/2.ce604390.chunk.js"
+  },
+  {
+    "revision": "810d8333fb06f114c011f6736d6a50fe",
+    "url": "./index.html"
+  }
+];
